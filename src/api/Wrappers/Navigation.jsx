b5ea0a82230f3041d87dc18ff645ebe62eacae14
xStarxLordx <lopezfrancisco1506@gmail.com>
2023-09-01 22:01:31 -0500

    cambios2 01/09/2023

--- src/api/Wrappers/Navigation.jsx
+++ src/api/Wrappers/Navigation.jsx
@@ -1,10 +1,16 @@
 import {createStackNavigator} from "@react-navigation/stack"
 import { NavigationContainer } from "@react-navigation/native";
+import { ROUTES } from "../constants/navigation.constant";
 
 const Stack = createStackNavigator();
 
 function Navigation(params) {
+    return <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen component={} mane = {ROUTES.sigUp}></Stack.Screen>
+        </Stack.Navigator>
 
+    </NavigationContainer>
 }
 
 export default Navigation
